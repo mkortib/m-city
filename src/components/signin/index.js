4c668f69104428aa1de90558ca6f7a1df2164219
mkortib <mkobria@gmail.com>
2019-07-21 18:15:29 +0300

    add demo auth

--- src/components/signin/index.js
+++ src/components/signin/index.js
@@ -102,6 +102,10 @@ export default class SignIn extends Component {
 					<form onSubmit={(event) => this.submitForm(event)}>
 						
 						<h2>Please Login</h2>
+					  <div className="demo_login">
+							<div>demo login: user@gmail.com</div>
+							<div>demo password: test_user</div>
+						</div>
 
 						<FormField 
 							id={'email'}
